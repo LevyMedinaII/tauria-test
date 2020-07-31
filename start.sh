b436c76cc1b493499a1a4ba10c569f6ecd7a951a
LevyMedinaII <levymedina3@gmail.com>
2020-07-31 20:23:41 +0800

    fix

---
+++ start.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+case $1 in
+    local)
+        echo "Running application in local development mode"
+        eval "docker-compose -f docker-compose.local.yml up --build"
+        ;;
+    prod)
+        echo "Running application in production mode"
+        eval "docker-compose -f docker-compose.prod.yml up --build -d"
+        ;;
+    *)
+        echo "Invalid environment. Choose from local or prod"
+        ;;
+esac
